5baa9b5c45823f2338ee3d1fb25223c791ba5fd0
Caleb Ngai <caleb.ngai@flosports.tv>
2022-03-09 14:18:18 -0600

    refactor code

--- Calculator/OperationManager.swift
+++ Calculator/OperationManager.swift
@@ -27,5 +27,7 @@ class OperationManager {
             return 0
         }
     }
+    
+    
 
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -21,106 +21,74 @@ class ViewController: UIViewController {
     var num2 = 0.00
     var result = 0.00
     
+    
 //MARK: - IBActions Arithmetic
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
         isFinishedTypingNumber = true
         decimalPointCount = 0
+        arithmeticOperation(senderTitle: sender.currentTitle!)
+    }
+    
+//MARK: - IBAction Modifier
+    @IBAction func modifierPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        modifierOperation(senderTitle: sender.currentTitle!)
+    }
+    
+//MARK: - Methods
         
-        switch sender.currentTitle {
-        case "+":
-            if num1 == 0.00{
-                num1 = Double(displayLabel.text!)!
-                operationKey = "+"
-            } else if operationIsDone{
-                operationKey = "+"
-                operationIsDone = false
-            }
-            else{
-                calculation()
-                operationKey = "+"
-            }
-        case "-":
-            if num1 == 0.00{
-                num1 = Double(displayLabel.text!)!
-                operationKey = "-"
-            }
-            else if operationIsDone{
-                operationKey = "-"
-                operationIsDone = false
-            }else {
-                calculation()
-                operationKey = "-"
-            }
-        case "×":
-            if num1 == 0.00{
-                num1 = Double(displayLabel.text!)!
-                operationKey = "×"
-            } else if operationIsDone{
-                operationKey = "×"
-                operationIsDone = false
-            }else {
-                calculation()
-                operationKey = "×"
-            }
-        case "÷":
-            if num1 == 0.00{
-                num1 = Double(displayLabel.text!)!
-                operationKey = "÷"
-            } else if operationIsDone{
-                operationKey = "÷"
-                operationIsDone = false
-            } else {
-                calculation()
-                operationKey = "÷"
-            }
-        case "=":
+    func calculation () {
+        num2 = Double(displayLabel.text!)!
+        result = operationManager.executeOperation(num1: num1, num2: num2, operationKey)
+        displayLabel.text = result.clean
+        num1 = result
+        num2 = 0.00
+    }
+    
+    func arithmeticOperation(senderTitle: String) {
+        if senderTitle == "="{
             calculation()
             operationIsDone = true
-        case "AC":
+        } else if senderTitle == "AC"{
             result = 0.00
             num1 = 0.00
             num2 = 0.00
             decimalPointCount = 0
             displayLabel.text = "0"
-            
-        default:
-            displayLabel.text = "error"
+        } else {
+            if num1 == 0.00{
+                num1 = Double(displayLabel.text!)!
+                operationKey = senderTitle
+            } else if operationIsDone{
+                operationKey = senderTitle
+                operationIsDone = false
+            }
+            else{
+                calculation()
+                operationKey = senderTitle
+            }
         }
     }
-//MARK: - IBAction Modifier
-    @IBAction func modifierPressed(_ sender: UIButton) {
-
-        isFinishedTypingNumber = true
+    
+    func modifierOperation(senderTitle: String) {
+        var calc: Double = 1.00
+        if senderTitle == "+/-" {calc = -1.00}
+        else {calc = 0.01}
         
-        
-        switch sender.currentTitle {
-            case "+/-":
-                if num1 == 0.00 {
-                    num1 = Double(displayLabel.text!)!
-                    num1 *= -1
-                    operationIsDone = true
-                    displayLabel.text = "\(num1.clean)"
-                }else {
-                    num2 = Double(displayLabel.text!)!
-                    num2 *= -1
-                    operationIsDone = true
-                    displayLabel.text = "\(num2.clean)"
-                }
-            default:
-                if num1 == 0.00 {
-                    num1 = Double(displayLabel.text!)!
-                    num1 /= 100.00
-                    operationIsDone = true
-                    displayLabel.text = "\(num1.clean)"
-                }else {
-                    num2 = Double(displayLabel.text!)!
-                    num2 /= 100.00
-                    operationIsDone = true
-                    displayLabel.text = "\(num2.clean)"
-                }
+        if num1 == 0.00 {
+            num1 = Double(displayLabel.text!)!
+            num1 *= calc
+            operationIsDone = true
+            displayLabel.text = "\(num1.clean)"
+        }else {
+            num2 = Double(displayLabel.text!)!
+            num2 *= calc
+            operationIsDone = true
+            displayLabel.text = "\(num2.clean)"
         }
+       
     }
+//MARK: - IBAction Number Buttons
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
@@ -134,32 +102,19 @@ class ViewController: UIViewController {
                 
                 if sender.currentTitle == "." && decimalPointCount < 1{
                     decimalPointCount = 1
-                    print(decimalPointCount)
                 }else if sender.currentTitle == "." && decimalPointCount == 1 {
                     print("too many decimals")
                     return
                 }
-                
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
     }
-
-//MARK: - Methods
-    func calculation () {
-        num2 = Double(displayLabel.text!)!
-        result = operationManager.executeOperation(num1: num1, num2: num2, operationKey)
-        displayLabel.text = result.clean
-        num1 = result
-        num2 = 0.00
-        
-    }
-
-
 }
 
 //MARK: - Double Extension
 extension Double {
+    
     var clean: String {
        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
     }
