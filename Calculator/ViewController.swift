244ac7e5b257bd8b2c58a48fc105374d30b2ef01
Caleb Ngai <caleb.ngai@flosports.tv>
2022-03-09 11:47:12 -0600

    add logic around decimal button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,15 +15,17 @@ class ViewController: UIViewController {
     
     var isFinishedTypingNumber: Bool = true
     var operationIsDone: Bool = false
+    var decimalPointCount = 0
     var operationKey: String = ""
     var num1 = 0.00
     var num2 = 0.00
     var result = 0.00
     
-//MARK: - IBActions
+//MARK: - IBActions Arithmetic
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         isFinishedTypingNumber = true
+        decimalPointCount = 0
         
         switch sender.currentTitle {
         case "+":
@@ -79,36 +81,39 @@ class ViewController: UIViewController {
             result = 0.00
             num1 = 0.00
             num2 = 0.00
+            decimalPointCount = 0
             displayLabel.text = "0"
             
         default:
             displayLabel.text = "error"
         }
     }
+//MARK: - IBAction Modifier
     @IBAction func modifierPressed(_ sender: UIButton) {
 
         isFinishedTypingNumber = true
         
+        
         switch sender.currentTitle {
             case "+/-":
                 if num1 == 0.00 {
-                num1 = Double(displayLabel.text!)!
-                num1 *= -1
-                operationIsDone = true
-                displayLabel.text = "\(num1.clean)"
-            } else {
-                num2 = Double(displayLabel.text!)!
-                num2 *= -1
-                operationIsDone = true
-                displayLabel.text = "\(num2.clean)"
-            }
+                    num1 = Double(displayLabel.text!)!
+                    num1 *= -1
+                    operationIsDone = true
+                    displayLabel.text = "\(num1.clean)"
+                }else {
+                    num2 = Double(displayLabel.text!)!
+                    num2 *= -1
+                    operationIsDone = true
+                    displayLabel.text = "\(num2.clean)"
+                }
             default:
                 if num1 == 0.00 {
                     num1 = Double(displayLabel.text!)!
                     num1 /= 100.00
                     operationIsDone = true
                     displayLabel.text = "\(num1.clean)"
-                } else {
+                }else {
                     num2 = Double(displayLabel.text!)!
                     num2 /= 100.00
                     operationIsDone = true
@@ -116,8 +121,6 @@ class ViewController: UIViewController {
                 }
         }
     }
-    
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
@@ -128,9 +131,17 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else{
+                
+                if sender.currentTitle == "." && decimalPointCount < 1{
+                    decimalPointCount = 1
+                    print(decimalPointCount)
+                }else if sender.currentTitle == "." && decimalPointCount == 1 {
+                    print("too many decimals")
+                    return
+                }
+                
                 displayLabel.text = displayLabel.text! + numValue
             }
-            
         }
     }
 
